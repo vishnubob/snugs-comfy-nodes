e7bbf2619c6c4a796ae23c90e539a05385040ac0
Giles Hall <giles@polymerase.org>
2024-01-23 15:21:18 -0500

    new approach to masking

--- __init__.py
+++ __init__.py
@@ -24,30 +24,37 @@ class ImageRemoveBackgroundNode:
             'required': {
                 'image': ('IMAGE',),
                 'model': (REMBG_MODELS, {'default': 'u2net'}),
-                'only_mask': (YES_NO, {'default': NO}),
                 'post_process_mask': (YES_NO, {'default': NO}),
             },
         }
 
-    RETURN_TYPES = ('IMAGE',)
+    RETURN_TYPES = ('IMAGE', 'MASK')
     FUNCTION = 'remove_background'
     CATEGORY = 'image'
 
-    def remove_background(self, image, model, only_mask, post_process_mask):
-        only_mask = yes_no_to_bool(only_mask)
+    def remove_background(self, image, model, post_process_mask):
         post_process_mask = yes_no_to_bool(post_process_mask)
+        image = image.cpu().numpy()
+        image_dtype = image.dtype
+        image = np.clip(image * 0xFF, 0, 0xFF).astype(np.uint8)
+
         if self._CACHED_SESSION[0] != model:
             self._CACHED_SESSION[0] = model
             self._CACHED_SESSION[1] = rembg.new_session(model)
         rembg_session = self._CACHED_SESSION[1]
-        rembg_output = rembg.remove(
-            image.cpu().numpy(),
-            only_mask=only_mask,
+        rembg_image = rembg.remove(
+            image,
+            putalpha=True,
             post_process_mask=post_process_mask,
             session=rembg_session
         )
-        rembg_output = torch.from_numpy(rembg_output)
-        return (rembg_output,)
+        rembg_image = np.clip(rembg_image / 0xFF, 0, 0xFF).astype(image_dtype)
+        rembg_mask = rembg_image[..., -1]
+        rembg_image = rembg_image[..., :3] * rembg_mask[..., np.newaxis]
+        rembg_image = torch.from_numpy(rembg_image[np.newaxis, ...])
+        rembg_mask = torch.from_numpy(rembg_mask[np.newaxis, ...])
+        return (rembg_image, rembg_mask)
+
 
 NODE_CLASS_MAPPINGS = {
     'Remove Background': ImageRemoveBackgroundNode
